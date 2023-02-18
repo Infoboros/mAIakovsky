e87f23629d7d24000446d699d751afddb6e142d3
infoboros <pintastd@gmail.com>
2023-02-19 01:35:58 +0300

    [add] log teaching for word2vec

--- language_models/teachers/word2vec.py
+++ language_models/teachers/word2vec.py
@@ -5,6 +5,8 @@ from os import path
 from gensim.models import Word2Vec
 from gensim.models.callbacks import CallbackAny2Vec
 
+import matplotlib.pyplot as plt
+
 from settings import RAW_EMBEDDING_ROOT, LANGUAGE_MODELS_ROOT
 from language_models.teachers.teacher import Teacher
 
@@ -12,14 +14,37 @@ from language_models.teachers.teacher import Teacher
 class LogCallback(CallbackAny2Vec):
     def __init__(self):
         self.epoch = 0
+
         self.loss_to_be_subbed = 0
+        self.loss_log = []
+
+        self.start = datetime.now()
+        self.end = datetime.now()
 
     def on_epoch_end(self, model):
         loss = model.get_latest_training_loss()
+
         loss_now = loss - self.loss_to_be_subbed
         self.loss_to_be_subbed = loss
+
         print('Loss after epoch {}: {}'.format(self.epoch, loss_now))
+        self.loss_log.append(loss_now)
         self.epoch += 1
+        self.end = datetime.now()
+
+    def print_statistic(self):
+        time_delta = self.end - self.start
+        print(f'Время обучения: {time_delta.seconds} секунд')
+
+        plt.plot(
+            [x for x in range(len(self.loss_log))],
+            self.loss_log
+        )
+
+        plt.title('Процесс обучения Word2Vec модели')
+        plt.ylabel('Ошибка')
+        plt.xlabel('Номер эпохи')
+        plt.show()
 
 
 class Word2VecTeacher(Teacher):
@@ -37,6 +62,7 @@ class Word2VecTeacher(Teacher):
 
     def fit(self):
         data = self.get_data()
+        log = LogCallback()
 
         self.model.build_vocab(
             data
@@ -46,10 +72,15 @@ class Word2VecTeacher(Teacher):
             total_examples=self.model.corpus_count,
             epochs=100,
             compute_loss=True,
-            callbacks=[LogCallback()]
+            callbacks=[log]
         )
 
         print(f'Размер словаря: {len(self.model.wv.index_to_key)} слов')
+        log.print_statistic()
+
+        start = datetime.now()
+        self.model.predict_output_word(['я', 'тебя', 'люблю'], topn=1000)
+        print(f'Время выполнения запроса: {(datetime.now() - start).microseconds} микросекунд')
 
     def save(self):
         raw_embedding_path = path.join(RAW_EMBEDDING_ROOT, self.dataset_name)
