ee69269f3a603bf983bf97af0707e60a6349f741
infoboros <pintastd@gmail.com>
2023-02-27 17:07:04 +0300

    [add] gpt rate in tg bot

--- bot/main.py
+++ bot/main.py
@@ -5,53 +5,70 @@ import telebot
 from generators import Word2VecGenerator
 from genetic.genetic import Genetic
 from genetic.simple import SimplePopulation
+from gpt import ChatGPT
 from language_models.ru_rap import Ru_rapModel
+
+
 # from meaning_classifier import MeaningClassifierCNNTaiga
 
 
 class Bot:
     def __init__(self, token: str):
-        print('Инициализаци...')
+        print('Инициализация...')
         self.bot = telebot.TeleBot(token)
+
+        print('Инициализация ChatGPT...')
+        self.gpt = ChatGPT()
+
+        print('Инициализация языковой модели')
+        self.embedding = Ru_rapModel()
+
+        # print('Инициализация классификатора')
+        # self.classifier = MeaningClassifierCNNTaiga()
+
+    def __generate_text(self, context: [str]) -> str:
+        prepared_context = list(map(
+            lambda word: word.lower(),
+            context
+        ))
+        generator = Word2VecGenerator(self.embedding, prepared_context)
+
+        five_yamb = '/__/_/__'
+        rows_count = 2
+        adam = generator.generate(metre_scheme=five_yamb, rows_count=rows_count)
+        eve = generator.generate(metre_scheme=five_yamb, rows_count=rows_count)
+
+        return '\n'.join([
+            ' '.join(map(str, row))
+            for row in [
+                adam[0],
+                eve[0],
+                adam[1],
+                eve[1]
+            ]
+        ])
+        # population = SimplePopulation([eve, adam, eve], embedding, classifier.classify_paragraph)
+        # genetic = Genetic(population)
+        # for generation in range(10):
+        #     print(f'Поколение {generation + 2}')
+        #     genetic.next_population()
+
+    def __rate_text(self, text: str) -> str:
+        return self.gpt.rate_poem(text)
 
     def definition_consumers(self):
         print('Объявление обработчиков...')
 
-        print('Инициализация языковой модели')
-        embedding = Ru_rapModel()
-
-        # print('Инициализация классификатора')
-        # classifier = MeaningClassifierCNNTaiga()
-
         @self.bot.message_handler(content_types=['text'])
         def process_text(message):
-            base_context = [word for word in message.text.split(' ')]
+            command, *text = message.text.split('\n')
 
-            generator = Word2VecGenerator(embedding, base_context)
+            if command.lower() == 'gen':
+                output = self.__generate_text(' '.join(text).split(' '))
+            else:
+                output = self.__rate_text('\n'.join([command, *text]))
 
-            five_yamb = '/__/_/__'
-            rows_count = 2
-            adam = generator.generate(metre_scheme=five_yamb, rows_count=rows_count)
-            eve = generator.generate(metre_scheme=five_yamb, rows_count=rows_count)
-
-            # population = SimplePopulation([eve, adam, eve], embedding, classifier.classify_paragraph)
-            # genetic = Genetic(population)
-            # for generation in range(10):
-            #     print(f'Поколение {generation + 2}')
-            #     genetic.next_population()
-
-            # FIXME
-            result = '\n'.join([
-                ' '.join(map(str, row))
-                for row in adam
-            ])
-            result += '\n'
-            result += '\n'.join([
-                ' '.join(map(str, row))
-                for row in eve
-            ])
-            self.bot.send_message(message.from_user.id, result)
-
+            self.bot.send_message(message.from_user.id, output)
 
     def start_polling(self):
         print('Запуск...')
---
+++ gpt/ChatGPT.py
@@ -0,0 +1,17 @@
+from settings import GPT_CHAT_TOKEN
+from pyChatGPT import ChatGPT as ChatGPTClient
+
+
+class ChatGPT:
+    def __init__(self):
+        self.api = ChatGPTClient(GPT_CHAT_TOKEN)
+
+    def rate_poem(self, poem: str) -> str:
+        return self.get_answer(
+            f'Оцени стихотворение\n'
+            f'{poem}'
+        )
+
+    def get_answer(self, text: str) -> str:
+        resp = self.api.send_message(text)
+        return resp['message']
